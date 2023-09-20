12cfdfd5d7bc95875230dcad099317019128328e
Akezhan Sotsial <107552938+Laistos@users.noreply.github.com>
2023-09-20 09:47:24 +0600

    Create Lab-1_Sotsial_Akezhan_SIS-2134.py
    Completed almost all tasks

---
+++ Lab-1_Sotsial_Akezhan_SIS-2134.py
@@ -0,0 +1,73 @@
+#Task 1
+# #1.1
+# arr = [4, 8, 15, 16, 23, 42]
+# print(arr[0], arr[1], arr[2], arr[3], arr[4], arr[5])
+
+#1.2
+# print(f'4','\n8', '\n15', '\n16', '\n23', '\n42')
+
+#1.3
+# a = int(input())
+# b = a + 1
+# c = a + 2
+# print(a)
+# print(b)
+# print(c)
+
+# #1.4
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# print(a + b + c)
+
+#1.5
+# a = int(input())
+# vol = a**3
+# S = 6 * a**2
+# print(f'Volume = ', vol)
+# print(f'Total surface area = ', S)
+
+#Task 2
+# #2.1
+# n = int(input())
+# k = int(input())
+# get = k / n
+# remain = k % n
+# print(get)
+# print(remain)
+
+# #2.2
+# a = input()
+# print(f'The digit in the thousands position is ', a[0])
+# print(f'The number in the hundreds position  is ', a[1])
+# print(f'The digit in the tens position is ', a[2])
+# print(f'The digit in the position of units is  ', a[3])
+
+# #2.3  
+# import math
+# n = int(input())
+# print(math.ceil(n/2))
+
+# 2.4
+n = int(input())
+if (n == 0) :
+   print('Error')
+else: print(f'The result of << is ', n + 1)
+
+#2.5
+# print(f"Please enter the first number: " )
+# a = int(input())
+# print(f"Please enter the second number: " )
+# b = int(input())
+# print(f"Please choose the operation (+, -, *, /): ")
+# operation = str(input())
+# match operation:
+#    case "+": 
+#       print(a + b)
+#    case "-":
+#       print(a - b)
+#    case "*":
+#       print(a * b)
+#    case "/":
+#       print(a / b)
+
